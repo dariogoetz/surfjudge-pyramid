1ec342694c408a3319dc95e694670a016743868f
Dario Goetz <dario.goetz@googlemail.com>
2018-11-26 18:21:23 +0100

    [fix] working drop offs also for different heat

--- surfjudge/static/surfjudge/js/heatchart.js
+++ surfjudge/static/surfjudge/js/heatchart.js
@@ -784,8 +784,6 @@
 
             })
             .on('end', function(participant){
-                console.log('end');
-                console.log(participant);
                 var hover_node = dragstate['hover_dropoff'];
                 if (hover_node === null){
                     console.log('dropped off outside');
@@ -796,115 +794,120 @@
 
                 //reset();
                 //_this.d3_heats.draw();
-                var old_heat_id = participant['node']['heat_data']['id'];
-                var old_seed = participant['seed'];
-                var heat_id = hover_node['heat']['heat_data']['id'];
-                var seed = hover_node['between_seeds'][1];
+                var from_heat_id = participant['node']['heat_data']['id'];
+                var from_seed = participant['seed'];
+                var to_heat_id = hover_node['heat']['heat_data']['id'];
+                var to_seed = hover_node['between_seeds'][1];
                 var new_part_data = {
                     surfer_id: participant['participant']['surfer_id'],
                     // surfer_color will be set by backend
-                    seed: seed,
-                    heat_id: heat_id,
+                    seed: to_seed,
+                    heat_id: to_heat_id,
                 };
 
-                if (old_heat_id == heat_id) {
+                if (from_heat_id == to_heat_id) {
+                    // only make changes that actually do something
                     var changed_participants = [];
-                    console.log('from', old_seed, 'to', seed);
-                    if ((old_seed != seed) && (old_seed != seed - 1)) {
+                    if ((from_seed != to_seed) && (from_seed != to_seed - 1)) {
                         // participant is only shifted within heat
-                        var direction = 1;
 
-                        // participant gets moved from old_seed to new_seed
+                        // participant gets moved from from_seed to to_seed
                         // all participants in between get a shift up or down
-                        // depending on whether the participant gets shifted down
-                        // or up
-                        var lower_change = seed;
-                        var upper_change = old_seed;
-                        if (old_seed < seed){
+                        // depending on whether the participant gets shifted
+                        // down (direction -1) or up (direction 1)
+                        var direction = 1;
+                        var lower_change = to_seed;
+                        var upper_change = from_seed;
+                        if (from_seed < to_seed){
+                            // seed and upper change need to be reduced by one
+                            // because the participant that will be removed
+                            // needs to be reflected
                             direction = -1;
                             new_part_data['seed'] -= 1;
-                            lower_change = old_seed;
-                            upper_change = seed - 1;
+                            lower_change = from_seed;
+                            upper_change = to_seed - 1;
                         }
 
                         $.each(participant['node']['heat_data']['participations'], function(i, p){
                             var new_p = $.extend({}, p);
                             delete new_p['surfer_color'];
                             delete new_p['surfer_color_hex'];
-                            if (p['seed'] == participant['seed']) return;
+                            if (p['seed'] == from_seed) return;
 
                             if ((p['seed'] >= lower_change) && (p['seed'] <= upper_change)) {
-                                new_p['seed'] = new_p['seed'] + direction;
+                                new_p['seed'] += direction;
                             };
                             changed_participants.push(new_p);
                         });
                         changed_participants.push(new_part_data);
                         $.ajax({
                             type: 'PUT',
-                            url: '/rest/participants' + '/' + heat_id,
+                            url: '/rest/participants' + '/' + to_heat_id,
                             data: JSON.stringify(changed_participants),
                         })
                         .done(function(){
                             reset();
                             _this.refresh();
                         })
-                        console.log('same heat');
-                        console.log(changed_participants);
                     } else {
-                        console.log("Nothing to do.")
+                        console.log("Nothing to do.");
+                        reset();
+                        _this.d3_heats.draw();
+                        return;
                     }
 
                 } else {
-                    console.log('different heats');
                     // determine participations of heat from which the participant came
                     // in particular, delete the participant and compress later seeds
                     var from_participants = [];
                     $.each(participant['node']['heat_data']['participations'], function(i, p){
-                        if (p['seed'] == participant['seed']) return;
+                        if (p['seed'] == from_seed) return;
                         var new_p = $.extend({}, p);
                         delete new_p['surfer_color'];
                         delete new_p['surfer_color_hex'];
-                        if (p['seed'] > participant['seed']) {
-                            new_p['seed'] = new_p['seed'] - 1;
+                        if (p['seed'] > from_seed) {
+                            new_p['seed'] -= 1;
                         }
                         from_participants.push(new_p);
                     });
-                    console.log(from_participants);
-
                     // determine participations of heat to which the participant goes
                     // in particular, shift the later seeds up
                     var to_participants = [];
                     $.each(hover_node['heat']['heat_data']['participations'], function(i, p){
-                        if (p['seed'] == participant['seed']) return;
                         var new_p = $.extend({}, p);
                         delete new_p['surfer_color'];
                         delete new_p['surfer_color_hex'];
-                        if (p['seed'] > participant['seed']) {
-                            new_p['seed'] = new_p['seed'] + 1;
+                        if (p['seed'] >= to_seed) {
+                            new_p['seed'] += 1;
                         }
                         to_participants.push(new_p);
                     });
                     to_participants.push(new_part_data);
-                    console.log(to_participants);
+                    // upload participants of target heat
+                    var deferred_from_heat = $.Deferred();
+                    $.ajax({
+                        type: 'PUT',
+                        url: '/rest/participants' + '/' + from_heat_id,
+                        data: JSON.stringify(from_participants),
+                    })
+                    .done(function(){deferred_from_heat.resolve();})
+                    .fail(function(){deferred_from_heat.resolve();});
+                    //upload participants of source heat
+                    var deferred_to_heat = $.Deferred();
+                    $.ajax({
+                        type: 'PUT',
+                        url: '/rest/participants' + '/' + to_heat_id,
+                        data: JSON.stringify(to_participants),
+                    })
+                    .done(function(){deferred_to_heat.resolve();})
+                    .fail(function(){deferred_to_heat.resolve();});
+
+                    // reset view on finish
+                    $.when(deferred_from_heat, deferred_to_heat).then(function(){
+                        reset();
+                        _this.refresh();
+                    });
                 }
-                reset();
-
-                _this.d3_heats.draw();
-
-
-                // // TODO: delete old participation
-                // $.ajax({
-                //     url: _this.options.deleteparticipanturl + '/' + old_heat_id + '/' + old_seed + '?action=comp    ress',
-                //     type: 'DELETE',
-                // })
-                // .done(function(){
-                //     $.post(_this.options.addparticipanturl + '/' + heat_id + '/' + seed + '?action=insert',
-                //         JSON.stringify(data))
-                //     .done(function(){
-                //         _this.refresh();
-                //         reset();
-                //     });
-                // });
             });
 
             var svg_participants = this.svg_elem.selectAll('.heat_seed.with_participant');
