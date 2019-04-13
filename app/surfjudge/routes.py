bdbce9cdf7e44be2728db03142c82ca57abbe967
Dario Goetz <dario.goetz@googlemail.com>
2019-04-13 14:26:25 +0200

    [mod] renamed default.py to resuts.py

--- app/surfjudge/routes.py
+++ app/surfjudge/routes.py
@@ -18,10 +18,10 @@ def includeme(config):
     config.add_route('test_forbidden', '/auth/test_forbidden')
 
     # public page routes
-    config.add_route('home', '/')  # views/default.py
+    config.add_route('home', '/')  # views/results.py
 
     # commentator page routes
-    config.add_route('commentator', '/commentator')  # views/default.py
+    config.add_route('commentator', '/commentator')  # views/results.py
 
     # admin page routes
     config.add_route('edit_tournaments', '/edit_tournaments')  # views/tournament.py
