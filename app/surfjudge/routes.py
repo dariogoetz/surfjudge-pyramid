7baf766fdfc7e059c05fe6160e75ce9fe41beb56
Dario Götz <dario.goetz@googlemail.com>
2019-04-15 08:28:17 +0000

    [mod] add home route to routes.py

--- app/surfjudge/routes.py
+++ app/surfjudge/routes.py
@@ -18,6 +18,7 @@ def includeme(config):
     config.add_route('test_forbidden', '/auth/test_forbidden')
 
     # public page routes
+    config.add_route('home', '/')  # views/result.py
     config.add_route('live_results', '/')  # views/result.py
     config.add_route('heatcharts', '/heatcharts')  # views/result.py
 
