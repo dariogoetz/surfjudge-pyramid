54222f6943485e5d49cc652f541780f1eea885ee
Dario Goetz <goetz@atesio.de>
2019-05-09 13:17:21 +0200

    [add] option to specify host/ip in startup scripts

--- start_server_dev.sh
+++ start_server_dev.sh
@@ -1,4 +1,11 @@
 export PYTHONPATH=.
-export host_ip=$(hostname -I)
+if [ -z "$1" ]
+then
+    echo "No hostname for websockets specified. Trying 'hostname -I'."
+    export host_ip=$(hostname -I)
+else
+    export host_ip=$1
+fi
 export WEBSOCKET_URL="ws://${host_ip}:6544"
+echo "Using websocket url $WEBSOCKET_URL"
 cd app && ../env/bin/pserve development.ini --reload
--- start_server_prod.sh
+++ start_server_prod.sh
@@ -1,4 +1,10 @@
 export PYTHONPATH=.
-export host_ip=$(hostname -I)
+then
+    echo "No hostname for websockets specified. Trying 'hostname -I'."
+    export host_ip=$(hostname -I)
+else
+    export host_ip=$1
+fi
 export WEBSOCKET_URL="ws://${host_ip}:6544"
+echo "Using websocket url $WEBSOCKET_URL"
 cd app && sudo ../env/bin/gunicorn --paste production.ini
