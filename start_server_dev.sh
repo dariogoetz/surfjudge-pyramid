fe2db3c3a173d856698dcd723728b871fe081763
Dario Goetz <dario.goetz@googlemail.com>
2019-05-12 19:51:39 +0200

    [mod] more verbose start scripts

--- start_server_dev.sh
+++ start_server_dev.sh
@@ -1,8 +1,16 @@
+echo "Starting development server"
 export PYTHONPATH=.
 if [ -z "$1" ]
 then
-    echo "No hostname for websockets specified. Trying 'hostname -I'."
+    echo "No hostname for websockets specified. Trying 'hostname -I'"
     export host_ip=$(hostname -I)
+    if [ -z "$host_ip" ]
+    then
+        echo "Found no IP! Using 'localhost'"
+        export host_ip=localhost
+    else
+        echo "Found IP '$host_ip'"
+    fi
 else
     export host_ip=$1
 fi
