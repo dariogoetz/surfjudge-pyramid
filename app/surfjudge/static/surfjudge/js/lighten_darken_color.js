af106fa973c9297eb5ba7c90d5adb54f92d4a9f4
Dario Goetz <dario.goetz@googlemail.com>
2019-05-25 18:11:10 +0200

    [add] luminance calculation

--- app/surfjudge/static/surfjudge/js/lighten_darken_color.js
+++ app/surfjudge/static/surfjudge/js/lighten_darken_color.js
@@ -30,3 +30,31 @@ function lighten_darken_color(col, amt) {
 function lighten(col) {
     return lighten_darken_color(col, 175);
 }
+
+function get_rgb_components(color) {
+
+    var r = color.substring(1, 3);
+    var g = color.substring(3, 5);
+    var b = color.substring(5, 7);
+
+    return [parseInt(r, 16), parseInt(g, 16), parseInt(b, 16)];
+}
+
+function hex_to_luminance(color) {
+    var rgb = get_rgb_components(color);
+
+    for (var i = 1; i < 3; i ++) {
+        rgb[i] = rgb[i] / 255.0;
+        if (rgb[i] <= 0.03928) {
+            rgb[i] = rgb[i] / 12.92;
+        } else {
+            rgb[i] = ((rgb[i] + 0.055) / 1.055) ^ 2.4;
+        }
+    }
+    var L = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+    return L
+}
+
+function text_color_for_background(color) {
+    return hex_to_luminance(color) < 0.179 ? 'white' : 'black'
+}
